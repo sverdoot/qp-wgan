427261ea908309c9a1b0e51a9847e25b0d37e7cc
burnyshev <pavel.burnysheff@yandex.ru>
2021-03-20 17:57:34 +0000

    mnist_exp

--- bin/mnist.sh
+++ bin/mnist.sh
@@ -4,6 +4,7 @@ BATCH_SIZE=300
 SEED=100
 REG_COEF_1=0
 REG_COEF_2=0
+N_EPOCH=40
 
 DATE=$(date +%H%M%S-%d%m)
 mkdir -p ../models
@@ -24,10 +25,34 @@ for p in 1 2; do
                 --seed ${SEED} \
                 --reg_coef1 ${REG_COEF_1} \
                 --reg_coef2 ${REG_COEF_2} \
-                --search_space x 
+                --search_space x \
+                --n_epoch ${N_EPOCH}
 
         done
     done
 done
 
+for p in 1.2; do
+    for q in 1.2; do
+        for n_critic in 1 5; do
+        
+            echo q: ${q} p: ${p} critic iterations: ${n_critic} 
+            
+            PYTHONPATH=. python src/main.py \
+                --task mnist \
+                --q ${q} \
+                --p ${p} \
+                --n_critic_iter ${n_critic} \
+                --batch_size ${BATCH_SIZE} \
+                --seed ${SEED} \
+                --reg_coef1 ${REG_COEF_1} \
+                --reg_coef2 ${REG_COEF_2} \
+                --search_space x \
+                --n_epoch ${N_EPOCH}
+
+        done
+    done
+done
+
+PYTHONPATH=. python src/plot_nearest_distance.py
 
